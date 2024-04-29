61aa7a2d0c76b64565625296819c5763c6314c17
kemthopaul <paulzk08@gmail.com>
2024-04-29 16:17:56 +0100

    ajout d'un plat

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-CMD /jena-fuseki/fuseki-server --file=/jena-fuseki/webapp/build/ontologie.rdf /FoodOntologie & npm start --prefix fuseki/build/
+CMD /jena-fuseki/fuseki-server --file=/jena-fuseki/webapp/build/ontologie.rdf /FoodOntologie & npm start --prefix /jena-fuseki/webapp/build/
